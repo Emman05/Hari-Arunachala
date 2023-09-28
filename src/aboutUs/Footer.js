bd395f8da2f1b4431e30c6f05eafd25306f3c882
emman05 <emmancool05@gmail.com>
2023-09-28 14:22:59 +0530

    updated

--- src/aboutUs/Footer.js
+++ src/aboutUs/Footer.js
@@ -2,49 +2,65 @@ import React from 'react';
 import Copyrights from './Copyrights';
 
 const Footer = () => {
-    const  Links=[
-        {
-            tittle:"column one",
-            link1:"link one",
-            link2:"link two",
-            link3:"link three",
-            link4:"link four",
-            link5:"link five",
-        },
-        {
-            tittle:"column two",
-            link1:"link one",
-            link2:"link two",
-            link3:"link three",
-            link4:"link four",
-            link5:"link five",
-        },
-       
-    ]
+  const Links = [
+    {
+      link1: "About Us",
+      link2: "Terms of Services",
+      link3: "Social Initiatives",
+      link4: "Terms And Conditions",
+      link5: "LinkedIn",
+    },
+    {
+      link1: "link one",
+      link2: "link two",
+      link3: "link three",
+      link4: "link four",
+      link5: "link five",
+    },
+  ];
+
   return (
-    <div className=' about-footer'>
+    <div className='about-footer'>
       <section className='footer-section'>
+        <div className='footer-container'>
+          {Links.map((item, index) => (
+            <ul key={index} className='footer-links'>
+              <h6>{item.tittle}</h6>
+              <li> <a href="./about">{item.link1}</a></li>
+              <li> <a href="#">{item.link2}</a></li>
+              <li> <a href="./social-initiatives">{item.link3}</a></li>
+              <li> <a href="#">{item.link4}</a></li>
+              <li> <a href="https://www.linkedin.com/company/yugaastech/">{item.link5}</a></li>
+            </ul>
+          ))}
         
-      <div className='footer-container'>
-       
         
-        {Links.map((item, index) => (
-          <ul key={index} className='footer-links'>
-            
-            <h6>{item.tittle}</h6>
-            <li> <a href="#">{item.link1}</a></li>
-            <li> <a href="#">{item.link2}</a></li>
-            <li> <a href="#">{item.link3}</a></li>
-            <li> <a href="#">{item.link4}</a></li>
-            <li> <a href="#">{item.link5}</a></li>
-            
-          </ul>
-        ))}
-      </div>
-      <Copyrights></Copyrights>
+        <div className="map-and-contents">
+          {/* Embed Google Maps as a map preview */}
+          <div className="map-preview">
+            <iframe
+              title="Map Preview"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124333.65310658963!2d79.9015071808839!3d13.135465684541018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5289b72cf8bb8f%3A0xd5fed379d9b04ecc!2sJaya%20Engineering%20College!5e0!3m2!1sen!2sin!4v1695884570970!5m2!1sen!2sin"
+              width="400"
+              height="300"
+              frameBorder="0"
+              style={{ border: '0' }}
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            ></iframe>
+          </div>
+        </div>
+          {/* Contents */}
+          <div className="footer-contents">
+            {/* Place your contents here */}
+          </div>
+        </div>
+        
+        <Copyrights />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
